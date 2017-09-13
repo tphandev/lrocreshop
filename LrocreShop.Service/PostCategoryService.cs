9cc346175b28741bcf53b369be20e991bc55c261
DESKTOP-QCEEE2B\tphan <tphan.dev@gmail.com>
2017-09-13 20:01:03 +0700

    add post, postCategory service

---
+++ LrocreShop.Service/PostCategoryService.cs
@@ -0,0 +1,71 @@
+﻿using LrocreShop.Data.Infrastructure;
+using LrocreShop.Data.Repositories;
+using LrocreShop.Model.Models;
+using System.Collections.Generic;
+
+namespace LrocreShop.Service
+{
+    public interface IPostCategoryService
+    {
+        void Add(PostCategory postCategory);
+
+        void Update(PostCategory postCategory);
+
+        void Delete(int id);
+
+        IEnumerable<PostCategory> GetAll();
+
+        IEnumerable<PostCategory> GetAllByParentId(int parentId);
+
+        PostCategory GetByID(int id);
+
+        void SaveChanges();
+    }
+
+    public class PostCategoryService : IPostCategoryService
+    {
+        private IPostCategoryRepository _postCategoryRepository;
+        private IUnitOfWork _unitOfWork;
+
+        public PostCategoryService(IPostCategoryRepository postCategoryRepository, IUnitOfWork unitOfWork)
+        {
+            this._postCategoryRepository = postCategoryRepository;
+            this._unitOfWork = unitOfWork;
+        }
+
+        public void Add(PostCategory postCategory)
+        {
+            _postCategoryRepository.Add(postCategory);
+        }
+
+        public void Delete(int id)
+        {
+            _postCategoryRepository.Delete(id);
+        }
+
+        public IEnumerable<PostCategory> GetAll()
+        {
+            return _postCategoryRepository.GetAll();
+        }
+
+        public IEnumerable<PostCategory> GetAllByParentId(int parentId)
+        {
+            return _postCategoryRepository.GetMulti(x => x.Status && x.ParentID == parentId);
+        }
+
+        public PostCategory GetByID(int id)
+        {
+            return _postCategoryRepository.GetSingleById(id);
+        }
+
+        public void SaveChanges()
+        {
+            _unitOfWork.Commit();
+        }
+
+        public void Update(PostCategory postCategory)
+        {
+            _postCategoryRepository.Update(postCategory);
+        }
+    }
+}
--- LrocreShop.Service/PostService.cs
+++ LrocreShop.Service/PostService.cs
@@ -16,6 +16,7 @@ namespace LrocreShop.Service
         void Delete(int id);
         IEnumerable<Post> GetAll();
         IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow);
+        IEnumerable<Post> GetAllByCategoryPaging(int categoryID,int page, int pageSize, out int totalRow);
         Post GetByID(int id);
         IEnumerable<Post> GetAllByTagPaging(string tag, int page, int pageSize, out int totalRow);
         void SaveChanges();
@@ -44,10 +45,15 @@ namespace LrocreShop.Service
            return _postRepository.GetAll(new string[] { "PostCategory" });
         }
 
+        public IEnumerable<Post> GetAllByCategoryPaging(int categoryID, int page, int pageSize, out int totalRow)
+        {
+            return _postRepository.GetMultiPaging(x => x.Status && x.CategoryID == categoryID, out totalRow, page, pageSize, new string[] { "PostCategory" });
+        }
+
         public IEnumerable<Post> GetAllByTagPaging(string tag, int page, int pageSize, out int totalRow)
         {
             //TODO: Select all post by tag
-            return _postRepository.GetMultiPaging(x => x.Status, out totalRow, page, pageSize);
+            return _postRepository.GetAllByTag(tag, page, pageSize, out totalRow);
         }
 
         public IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow)
