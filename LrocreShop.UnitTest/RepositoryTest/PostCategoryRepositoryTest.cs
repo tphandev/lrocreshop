8d46891d9934bf5fbb45a8a949a656b3706758c8
DESKTOP-QCEEE2B\tphan <tphan.dev@gmail.com>
2017-09-19 16:51:55 +0700

    Add unit test for repository and service, change return of method add and delete to T, change return of method GetAll to Ienumerable

---
+++ LrocreShop.UnitTest/RepositoryTest/PostCategoryRepositoryTest.cs
@@ -0,0 +1,46 @@
+﻿using LrocreShop.Data.Infrastructure;
+using LrocreShop.Data.Repositories;
+using LrocreShop.Model.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace LrocreShop.UnitTest.RepositoryTest
+{
+    [TestClass]
+    public class PostCategoryRepositoryTest
+    {
+        private IDbFactory dbFactory;
+        private IPostCategoryRepository objRepository;
+        private IUnitOfWork unitOfWork;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            dbFactory = new DbFactory();
+            objRepository = new PostCategoryRepository(dbFactory);
+            unitOfWork = new UnitOfWork(dbFactory);
+        }
+
+        [TestMethod]
+        public void PostCategory_Reposiotry_GetAll()
+        {
+            var list = objRepository.GetAll();
+            Assert.AreEqual(6, list.Count());
+        }
+
+        [TestMethod]
+        public void PostCategory_Repository_Create()
+        {
+            PostCategory category = new PostCategory();
+            category.Name = "Test Category";
+            category.Alias = "test-category";
+            category.Status = true;
+
+            var result = objRepository.Add(category);
+            unitOfWork.Commit();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(6, result.ID);
+        }
+    }
+}
