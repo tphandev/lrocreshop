71c217aa80b27cef91e25c97c1363804137ba08b
DESKTOP-QCEEE2B\tphan <tphan.dev@gmail.com>
2017-10-04 10:17:14 +0700

    add notificationService for list

--- LrocreShop.Web/app/components/product_categories/productCategoryListController.js
+++ LrocreShop.Web/app/components/product_categories/productCategoryListController.js
@@ -1,8 +1,8 @@
 ﻿(function (app) {
     app.controller('productCategoryListController', productCategoryListController);
 
-    productCategoryListController.$inject = ['$scope', 'apiService'];
-    function productCategoryListController($scope, apiService) {
+    productCategoryListController.$inject = ['$scope', 'apiService', 'notificationService'];
+    function productCategoryListController($scope, apiService, notificationService) {
         $scope.productCategories = [];
         $scope.page = 0;
         $scope.pageCount = 0;
@@ -21,10 +21,16 @@
                 params: {
                     keyword: $scope.keyword,
                     page: page,
-                    pageSize:2
+                    pageSize: 2
                 }
             }
             apiService.get('/api/productcategory/getall', config, function (result) {
+                if (result.data.TotalCount == 0) {
+                    notificationService.displayWarning('Không tìm thấy bản ghi nào');
+                }
+                else {
+                    notificationService.displaySuccess('Đã tìm thấy ' + result.data.TotalCount + ' bản ghi');
+                };
                 $scope.productCategories = result.data.Items;
                 $scope.page = result.data.Page;
                 $scope.pagesCount = result.data.TotalPages;
---
+++ LrocreShop.Web/app/shared/services/notificationService.js
@@ -0,0 +1,44 @@
+﻿(function (app) {
+
+    app.service('notificationService', notificationService);
+
+    function notificationService() {
+
+        toastr.option = {
+            "debug": false,
+            "positionClass": "toast-top-right",
+            "onlick": null,
+            "fadeIn": 300,
+            "fadeOut": 1000,
+            "timeOut": 1000,
+            "extendedTimeOut": 1000
+        }
+
+        function displaySuccess(message) {
+            toastr.success(message);
+        }
+        function displayError(error) {
+            if (Array.isArray(error)) {
+                error.each(function (err) {
+                    toastr.error(err);
+                });
+            }
+            else
+                toastr.success(error);
+        }
+        function displayInfo(message) {
+            toastr.info(message);
+        }
+        function displayWarning(message) {
+            toastr.warning(message);
+        }
+
+        return {
+            displaySuccess: displaySuccess,
+            displayError: displayError,
+            displayInfo: displayInfo,
+            displayWarning: displayWarning
+        }
+    };
+
+})(angular.module('lrocreshop.common'));
