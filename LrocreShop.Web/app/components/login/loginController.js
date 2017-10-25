512e25e387d69ca29677c1b447fe11289e1d1bdb
DESKTOP-QCEEE2B\tphan <tphan.dev@gmail.com>
2017-10-25 16:04:58 +0700

    add login form and implement nested template

---
+++ LrocreShop.Web/app/components/login/loginController.js
@@ -0,0 +1,13 @@
+﻿(function (app) {
+
+    app.controller('loginController', loginController);
+
+    loginController.$inject=['$scope','$state']
+
+    function loginController($scope, $state) {
+        $scope.loginSubmit = function () {
+            $state.go('home')
+        }
+    };
+
+})(angular.module('lrocreshop'))
--- LrocreShop.Web/app/components/products/products.module.js
+++ LrocreShop.Web/app/components/products/products.module.js
@@ -8,14 +8,17 @@
     function config($stateProvider, $urlRouterProvider) {
         $stateProvider.state('products', {
             url: "/products",
+            parent: 'base',
             templateUrl: "/app/components/products/productListView.html",
             controller: "productListController"
         }).state('add_product', {
             url: "/add_product",
+            parent: 'base',
             templateUrl: "/app/components/products/productAddView.html",
             controller: "productAddController"
         }).state('edit_product', {
             url: "/edit_product/:id",
+            parent: 'base',
             templateUrl: "/app/components/products/productEditView.html",
             controller: "productEditController"
         });
