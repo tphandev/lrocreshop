6b1ee30b425c20e7749bf098e951e5c3eb05c41b
DESKTOP-QCEEE2B\tphan <tphan.dev@gmail.com>
2017-11-03 21:49:52 +0700

    routing config for client side

--- LrocreShop.Web/Controllers/HomeController.cs
+++ LrocreShop.Web/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace TeduShop.Web.Controllers
+namespace LrocreShop.Web.Controllers
 {
     public class HomeController : Controller
     {
